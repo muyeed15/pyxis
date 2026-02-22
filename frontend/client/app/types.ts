ca69c9a2baeae1809d9ae87f320e170f40e43e00
muyeed15 <muyeed.al.abdullah@gmail.com>
2026-02-22 19:45:00 +0600

    Added pagination key

--- frontend/client/app/types.ts
+++ frontend/client/app/types.ts
@@ -1,32 +1,29 @@
-//text
 export interface TextSearchResultItem {
   title: string;
   href: string;
   body: string;
 }
 
-//image
 export interface ImageSearchResultItem {
   title: string;
-  image: string;      
-  thumbnail: string;  
-  url: string;        
+  image: string;
+  thumbnail: string;
+  url: string;
   height: number;
   width: number;
   source: string;
 }
 
-// video
 export interface VideoSearchResultItem {
   title: string;
-  content: string;   
+  content: string;
   description?: string;
-  images?: {         
+  images?: {
     large?: string;
     medium?: string;
     small?: string;
     motion?: string;
-  } | string;        
+  } | string;
   duration?: string;
   publisher?: string;
   published?: string;
@@ -43,5 +40,7 @@ export interface APIResponse {
   search_type: string;
   query: string;
   count: number;
+  page?: number;
+  has_more?: boolean;
   results: TextSearchResultItem[] | ImageSearchResultItem[] | VideoSearchResultItem[];
 }
